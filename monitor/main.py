98e5f3967fe4d0342ae4f09aee98baa6c1b3e085
FoxeiZ <57582539+FoxeiZ@users.noreply.github.com>
2025-05-01 20:56:35 +0700

    add run script

---
+++ monitor/main.py
@@ -0,0 +1,29 @@
+import os
+import sys
+
+from lib.manager import PluginManager
+from plugin import (
+    InterfaceMonitorPlugin,
+    LongProcessPlugin,
+    LongProcessPluginWithError,
+    SystemMonitorPlugin,
+    SystemServerPlugin,
+)
+
+# __all__ = ["PluginManager", "InterfaceMonitorPlugin", "TailscalePlugin"]
+
+
+if __name__ == "__main__":
+    manager = PluginManager(
+        webhook_url=os.environ.get(
+            "WEBHOOK_URL",
+            sys.argv[1] if len(sys.argv) > 1 else None,
+        ),
+    )
+    manager.register_plugin(InterfaceMonitorPlugin)
+    manager.register_plugin(LongProcessPlugin)
+    manager.register_plugin(LongProcessPluginWithError)
+    # manager.register_plugin(LongProcessPluginWithLongOutput)
+    manager.register_plugin(SystemServerPlugin)
+    manager.register_plugin(SystemMonitorPlugin)
+    manager.run()
