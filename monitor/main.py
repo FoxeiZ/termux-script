be4540a2cb8f3c94afe72df7d7f1c57b14be2ec8
FoxeiZ <57582539+FoxeiZ@users.noreply.github.com>
2025-05-02 14:47:15 +0700

    load test plugin on DEBUG only

--- monitor/main.py
+++ monitor/main.py
@@ -20,10 +20,12 @@ if __name__ == "__main__":
             sys.argv[1] if len(sys.argv) > 1 else None,
         ),
     )
+    if os.environ.get("DEBUG", "0") == "1":
+        manager.register_plugin(LongProcessPlugin)
+        manager.register_plugin(LongProcessPluginWithError)
+        # manager.register_plugin(LongProcessPluginWithLongOutput)
+
     manager.register_plugin(InterfaceMonitorPlugin)
-    manager.register_plugin(LongProcessPlugin)
-    manager.register_plugin(LongProcessPluginWithError)
-    # manager.register_plugin(LongProcessPluginWithLongOutput)
     manager.register_plugin(SystemServerPlugin)
     manager.register_plugin(SystemMonitorPlugin)
     manager.run()
