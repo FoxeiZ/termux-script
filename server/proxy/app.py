e2a09c175341f4cf02c05442a0d771bd231bb2ad
FoxeiZ <57582539+FoxeiZ@users.noreply.github.com>
2025-08-02 03:45:56 +0700

    feat: debug specific arg

--- server/proxy/app.py
+++ server/proxy/app.py
@@ -8,9 +8,13 @@ def create_app() -> Quart:
     app.secret_key = "huhu"
     app.config["TEMPLATES_AUTO_RELOAD"] = True
     register_all_routes(app)
+
+    if app.debug:
+        app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
     return app
 
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
