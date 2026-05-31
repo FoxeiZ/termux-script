b32701f109fd9758fe646124a823d204ab0b33b5
FoxeiZ <57582539+FoxeiZ@users.noreply.github.com>
2026-05-31 22:47:46 +0700

    feat: update default pm2 path

--- pm2_scripts/nonroot/start.sh
+++ pm2_scripts/nonroot/start.sh
@@ -3,6 +3,6 @@
 BASE_DIR="/data/data/com.termux/files/home/scripts/pm2_scripts"
 SCRIPT_DIR="$BASE_DIR/root"
 
-export PM2_HOME="/data/data/com.termux/files/home/.pm2-nonroot"
+export PM2_HOME="/data/data/com.termux/files/home/.pm2"
 
 pm2 kill
--- pm2_scripts/root/start.sh
+++ pm2_scripts/root/start.sh
@@ -6,8 +6,9 @@ fi
 
 BASE_DIR="/data/data/com.termux/files/home/scripts/pm2_scripts"
 SCRIPT_DIR="$BASE_DIR/root"
+PYTHON_CMD="sudo -E python"
 
-export PM2_HOME="/data/data/com.termux/files/home/.pm2-root"
+export PM2_HOME="/data/data/com.termux/files/home/.suroot/.pm2"
 
 pm2 kill
 
@@ -15,10 +16,10 @@ pm2 kill
 # because `pm2` with `--interpreter` runs the script with and forks a child process, making us lose the root privileges
 # executing the script with `sudo` directly allows us to run the script with root privileges even if pm2 forks it
 # and `-E` is needed to preserve the environment variables, otherwise the script won't be able to find the modules
-pm2 start "sudo -E python $SCRIPT_DIR/system_monitor.py" --name "root-system-monitor" --cwd "$SCRIPT_DIR"
-pm2 start "sudo -E python $SCRIPT_DIR/system_server_monitor.py" --name "root-server-monitor" --cwd "$SCRIPT_DIR"
-pm2 start "sudo -E python $SCRIPT_DIR/process_watch_monitor.py" --name "root-process-monitor" --cwd "$SCRIPT_DIR"
-pm2 start "sudo -E python $SCRIPT_DIR/interface_monitor.py" --name "root-interface-monitor" --cwd "$SCRIPT_DIR"
-pm2 start "sudo -E python $SCRIPT_DIR/tailscale/tailscale.py" --name "root-tailscale" --cwd "$SCRIPT_DIR/tailscale"
+pm2 start "$PYTHON_CMD $SCRIPT_DIR/system_monitor.py" --name "root-system-monitor" --cwd "$SCRIPT_DIR"
+pm2 start "$PYTHON_CMD $SCRIPT_DIR/system_server_monitor.py" --name "root-server-monitor" --cwd "$SCRIPT_DIR"
+pm2 start "$PYTHON_CMD $SCRIPT_DIR/process_watch_monitor.py" --name "root-process-monitor" --cwd "$SCRIPT_DIR"
+pm2 start "$PYTHON_CMD $SCRIPT_DIR/interface_monitor.py" --name "root-interface-monitor" --cwd "$SCRIPT_DIR"
+pm2 start "$PYTHON_CMD $SCRIPT_DIR/tailscale/tailscale.py" --name "root-tailscale" --cwd "$SCRIPT_DIR/tailscale"
 
 pm2 save
